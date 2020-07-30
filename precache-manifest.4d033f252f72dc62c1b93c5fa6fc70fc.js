993a78fc5d20631c600f9cc2878118510df829ac
shalini-dhanasekar <shalini.1701221@srec.ac.in>
2020-07-30 20:27:56 +0530

    Updates

---
+++ precache-manifest.4d033f252f72dc62c1b93c5fa6fc70fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b248bfae6593d45c6ecfe4d0ea89f51",
+    "url": "/myntra-react/index.html"
+  },
+  {
+    "revision": "cdcef763c351bf2cd919",
+    "url": "/myntra-react/static/css/main.6a264f8a.chunk.css"
+  },
+  {
+    "revision": "0465d604edd310555409",
+    "url": "/myntra-react/static/js/2.a206118e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myntra-react/static/js/2.a206118e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdcef763c351bf2cd919",
+    "url": "/myntra-react/static/js/main.5a35e0c3.chunk.js"
+  },
+  {
+    "revision": "b47e678728639f1b7e0c",
+    "url": "/myntra-react/static/js/runtime-main.17dd0814.js"
+  },
+  {
+    "revision": "f0d1b18e86a614fa264930c50c5d4b79",
+    "url": "/myntra-react/static/media/myntralogo.f0d1b18e.png"
+  }
+]);
